8c1c750d66862fb4517aee10a235dd21630282ac
zxx <648482086@qq.com>
2021-06-02 06:39:08 +0800

    WebServer without timer and HttpServer

---
+++ WebServer/Channel.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+#include "Epoll.h"
+#include "EventLoop.h"
+#include <memory>
+#include <string>
+#include <functional>
+#include <sys/epoll.h>
+#include "noncopyable.h"
+
+class EventLoop;
+class HttpConn;
+
+class Channel : noncopyable {
+private:
+    typedef std::function<void()> CallBack; //fix bug.should ahead of class.
+    void updatePoller(Channel* channel);
+    void addtoPoller(Channel* channel);
+public:
+    Channel(EventLoop* loop, int fd);
+    Channel(EventLoop* loop);
+    ~Channel();
+public:
+    void setReadcallback(CallBack&& cb);
+    void setWritecallback(CallBack&& cb);
+    void setErrorcallback(CallBack&& cb);
+    void setConncallback(CallBack&& cb);
+    void setCloseCallBack(CallBack&& cb); //attention on this one
+public:
+    void handleRead();
+    void handleWrite();
+    void handleError();
+    void handleConn();
+    void handleEvents();
+    void handleClose();
+public:
+    void setEvents(__uint32_t events);
+    __uint32_t getEvents();
+    //void setlastEvents(__uint32_t event); //abandon, it's not a reference
+    //__uint32_t getlastEvents();
+public:
+    void setHolder(std::shared_ptr<HttpConn> httpconn);
+    std::shared_ptr<HttpConn> getHolder();
+public:
+    void setFd(int fd);
+    int getFd();
+    
+private:
+    __uint32_t events_;
+    __uint32_t lastevents_;
+    EventLoop* loop_;
+    int fd_;
+    std::weak_ptr<HttpConn> holder_;
+    CallBack readcallback_;
+    CallBack writecallback_;
+    CallBack errorcallback_;
+    CallBack conncallback_;
+    CallBack closecallback_;
+};
--- WebServer/EventLoop.h
+++ WebServer/EventLoop.h
@@ -3,18 +3,40 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <thread>
+#include "Epoll.h"
 #include "noncopyable.h"
+#include "MutexLock.h"
+#include "Condition.h"
+#include <sys/eventfd.h>
+#include <functional>
 
+class Epoll; //fix bug
+class Channel; //fix bug
 
 class EventLoop : noncopyable {
 public:
+    typedef std::function<void()> funcCallback;
     EventLoop();
     ~EventLoop();
     void loop();
-    void quit();  
+    void quit();
+    void runInLoop(funcCallback&& cb);
+    void doPendingFunctors();
 private:
     bool isloopInthisThread() const;
+    void handleRead();
+    void queueInLoop(funcCallback&& cb);
+    void wakeup();
+    int createEventFd();
+    void addtoPoller(Channel* channel);
     const std::thread::id threadId_;
     bool looping_;
     bool quit_;
+    MutexLock mutex_;
+    std::vector<funcCallback> pendingfunctors_;
+    bool callingPendingFunctors_;
+    std::shared_ptr<Channel> wakeupChannel_;
+    int wakeupFd_;
+public:
+    std::unique_ptr<Epoll> epoller_;
 };
